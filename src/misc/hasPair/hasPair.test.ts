ee941afd00a974d7cace75954507cf592ab68cac
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-07-23 11:01:26 +0100

    WIP

--- src/misc/hasPair/hasPair.test.ts
+++ src/misc/hasPair/hasPair.test.ts
@@ -4,11 +4,11 @@ import { hasPair } from './hasPair'
 describe('hasPair', () => {
   test('should return true if the array has a unique pair that adds up to the sum', () => {
     assert.equal(hasPair([1, 3, 7], 10), true)
-    assert.equal(hasPair([-5, -2, 1], -1), true)
-    assert.equal(hasPair([1, 5, 6, 9, 10, 14, 16, 20, 27, 30], 36), true)
+    // assert.equal(hasPair([-5, -2, 1], -1), true)
+    // assert.equal(hasPair([1, 5, 6, 9, 10, 14, 16, 20, 27, 30], 36), true)
   })
 
   test('should return false if the array does not have a unique pair that adds up to the sum', () => {
-    assert.equal(hasPair([1, 5, 6, 9, 10, 14, 16, 20, 27, 30], 3), false)
+    // assert.equal(hasPair([1, 5, 6, 9, 10, 14, 16, 20, 27, 30], 3), false)
   })
 })
--- src/misc/hasPair/hasPair.ts
+++ src/misc/hasPair/hasPair.ts
@@ -1,35 +1,14 @@
 export function hasPair(nums: number[], targetSum: number): boolean {
-  // Check if the array has a unique pair that adds up to the sum
+  const numSet = new Set<number>()
 
-  for (let index = 0; index < nums.length; index++) {
-    // Skip the last num
-    if (index === nums.length - 1) {
-      continue
+  for (const num of nums) {
+    const complement = targetSum - num
+
+    if (numSet.has(complement)) {
+      return true
     }
-    const num = nums[index]
-    const otherNums = nums.slice(index + 1)
 
-    // Compare number to each of the other numbers
-    for (
-      let otherNumIndex = 0;
-      otherNumIndex < otherNums.length;
-      otherNumIndex++
-    ) {
-      const otherNum = otherNums[otherNumIndex]
-
-      // Skip other number if it's the same as the current number
-      if (otherNum === num) {
-        continue
-      }
-
-      const sum = num + otherNum
-
-      console.log({ num, otherNum, sum })
-
-      if (sum === targetSum) {
-        return true
-      }
-    }
+    numSet.add(num)
   }
 
   return false
