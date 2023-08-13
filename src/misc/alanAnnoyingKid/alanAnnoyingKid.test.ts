927a083d6ccd636d8f9fc1154ef43269167e85a5
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-08-13 11:38:09 +0100

    WIP

--- src/misc/alanAnnoyingKid/alanAnnoyingKid.test.ts
+++ src/misc/alanAnnoyingKid/alanAnnoyingKid.test.ts
@@ -7,18 +7,20 @@ describe('alanAnnoyingKid', () => {
       "I don't think you played football today, I think you didn't play at all!"
     )
 
-    // expect(alanAnnoyingKid("Today I didn't play football.")).toEqual(
-    //   "I don't think you didn't play football today, I think you did play it!"
-    // )
-    //
-    // expect(
-    //   alanAnnoyingKid("Today I didn't attempt to hardcode this Kata.")
-    // ).toEqual(
-    //   "I don't think you didn't attempt to hardcode this Kata today, I think you did attempt it!"
-    // )
-    //
-    // expect(alanAnnoyingKid('Today I cleaned the kitchen.')).toEqual(
-    //   "I don't think you cleaned the kitchen today, I think you didn't clean at all!"
-    // )
+    expect(alanAnnoyingKid('Today I cleaned the kitchen.')).toEqual(
+      "I don't think you cleaned the kitchen today, I think you didn't clean at all!"
+    )
+  })
+
+  test('handles negating negative statements', () => {
+    expect(alanAnnoyingKid("Today I didn't play football.")).toEqual(
+      "I don't think you didn't play football today, I think you did play it!"
+    )
+
+    expect(
+      alanAnnoyingKid("Today I didn't attempt to hardcode this Kata.")
+    ).toEqual(
+      "I don't think you didn't attempt to hardcode this Kata today, I think you did attempt it!"
+    )
   })
 })
--- src/misc/alanAnnoyingKid/alanAnnoyingKid.ts
+++ src/misc/alanAnnoyingKid/alanAnnoyingKid.ts
@@ -1,13 +1,28 @@
-export function alanAnnoyingKid(input: string): string {
-  // 1. I don't think you `<past-tense-verb>` `<subject>`.
+export function alanAnnoyingKid(statement: string): string {
+  return statement.includes("didn't")
+    ? negateNegativeStatement(statement)
+    : negatePositiveStatement(statement)
+}
 
-  const words = input.split(' ')
+function negatePositiveStatement(statement: string) {
+  const words = statement.split(' ')
   const pastTenseVerb = words[2]
   const subject = words.slice(3).join(' ').replace('.', '')
   const presentTenseVerb = pastTenseVerb.replace('ed', '')
 
   const firstPart = `I don't think you ${pastTenseVerb} ${subject} today`
-  const secondPart = `I think you didn't ${presentTenseVerb} at all!`
+  const secondPart = `I think you didn't ${presentTenseVerb} at all`
 
-  return `${firstPart}, ${secondPart}`
+  return `${firstPart}, ${secondPart}!`
+}
+
+export function negateNegativeStatement(statement: string) {
+  const words = statement.split(' ')
+  const action = words.slice(2).join(' ').replace('.', '')
+  const verb = words[3]
+
+  const firstPart = `I don't think you ${action} today`
+  const secondPart = `I think you did ${verb} it`
+
+  return `${firstPart}, ${secondPart}!`
 }
